6daba80a4998b3483c29ca811ab7c84b159fdcbb
patevs <ppevans11@gmail.com>
2018-08-11 17:28:27 +1200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// react
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// font awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+// main style sheet
+import './index.css';
+
+// main nav bar
+import MainNav from './MainNav/MainNav';
+// jumbotron
+import Jumbo from './Jumbo/Jumbo';
+
+import registerServiceWorker from './registerServiceWorker';
+
+// add svg icons to font awesome library
+library.add(faStroopwafel);
+
+// render react components
+ReactDOM.render(<MainNav />, document.getElementById('main-nav'));
+ReactDOM.render(<Jumbo />, document.getElementById('jumbo'));
+
+registerServiceWorker();
